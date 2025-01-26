778d3903ac8d57676ce5fa6eac6bc99e9125546d
FeyEtros <134168682+FeyEtros@users.noreply.github.com>
2025-01-26 20:38:56 +0100

    path handling, convert function, small UI changes

--- pdf_manager.py
+++ pdf_manager.py
@@ -2,6 +2,9 @@ from pypdf import PdfReader
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
+import img2pdf
+from PIL import Image
+import os
 
 class MainApp(MDApp):
     def build(self):
@@ -22,9 +25,31 @@ class MainScreen(Screen):
     def selected(self, selection):
         print(selection[0])
         path = selection[0]
+        return path
 
-    def convert():
+    def convert(path):
+        img_path = path
+        image = Image.open(img_path)
+        pdf_bytes = img2pdf.convert(image.filename)
+        pdf_path = self.save_file
+        ###### Problem: wie den save path hier reinkriegen? Nicht auswählbar weil über save_file function, weil diese function zwingend in
+        ###### MainScreen class sein muss, Objekt aber noch nicht existiert weshalb save_file() nicht ausführbar ist.
+        file = open(pdf_path, 'wb')
+        file.write(pdf_bytes)
+        image.close()
+        file.close()
+
+    def save_file(self):
+        from plyer import filechooser
+        filechooser.save_file(on_selection = self.selected)
+        #print(selection[0])
+        #save_path = "selection[0]"
+        #return save_path
+        return self
+
+    def cancel():
         pass
+
 
     def merge():
         pass
