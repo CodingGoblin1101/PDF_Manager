1bfb3912f212ce1d8d1939c76dde42dc991e1224
FeyEtros <134168682+FeyEtros@users.noreply.github.com>
2025-02-15 20:17:00 +0100

    * stop opening second file choosing if first file choosing is interrupted * mid refactoring

--- pdf_manager.py
+++ pdf_manager.py
@@ -6,34 +6,26 @@ import img2pdf
 from PIL import Image
 import os
 
-#TODO: refactor IndexError handling // handle bug opening second window after closing first
-
-def convert(path, save_path):
-        image = Image.open(path[0])
-        pdf_bytes = img2pdf.convert(image.filename)
-        file = open(str(save_path[0]), 'wb')
-        file.write(pdf_bytes)
-        image.close()
-        file.close()
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Midnightblue'
         return Builder.load_file('pdf_manager.kv')
-    
+
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
     # **kwargs -> belieblig viele Argumente annehmbar, als dictionary wiedergegeben
     # super(). -> ruft die __init__ Methode der Eltern auf, bedeutet Eltern nehmen gleiche Argumente entgegen
-
     def select_file(self):
         from plyer import filechooser
         try:
             path = filechooser.open_file(on_selection = self.selected)
-            save_path = self.save_file()
-            convert(path, save_path)
+            print(path)
+            if path:
+                save_path = self.save_file()
+                self.convert(path, save_path)
         except IndexError:
             return
 
@@ -58,6 +50,16 @@ class MainScreen(Screen):
 
     def merge():
         pass
+
+    #TODO: refactor IndexError handling
+
+    def convert(self,path, save_path):
+        image = Image.open(path[0])
+        pdf_bytes = img2pdf.convert(image.filename)
+        file = open(str(save_path[0]), 'wb')
+        file.write(pdf_bytes)
+        image.close()
+        file.close()
 
     def split():
         pass
