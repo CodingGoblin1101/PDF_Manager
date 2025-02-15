c3896a5dcb51183a936ea87464b4b46d438eb692
FeyEtros <134168682+FeyEtros@users.noreply.github.com>
2025-02-15 20:44:50 +0100

    *change request logic *refactor IndexError handling

--- pdf_manager.py
+++ pdf_manager.py
@@ -6,6 +6,8 @@ import img2pdf
 from PIL import Image
 import os
 
+from setuptools.sandbox import save_path
+
 
 class MainApp(MDApp):
     def build(self):
@@ -18,32 +20,24 @@ class MainScreen(Screen):
         super().__init__(**kwargs)
     # **kwargs -> belieblig viele Argumente annehmbar, als dictionary wiedergegeben
     # super(). -> ruft die __init__ Methode der Eltern auf, bedeutet Eltern nehmen gleiche Argumente entgegen
+
     def select_file(self):
         from plyer import filechooser
-        try:
-            path = filechooser.open_file(on_selection = self.selected)
-            print(path)
-            if path:
-                save_path = self.save_file()
-                self.convert(path, save_path)
-        except IndexError:
-            return
+        path = filechooser.open_file(on_selection = self.selected)
+        if path:
+            return path
 
     def save_file(self):
         from plyer import filechooser
-        try:
-            save_path = (filechooser.save_file(on_selection = self.selected))
+        save_path = (filechooser.save_file(on_selection = self.selected))
+        if save_path:
             save_path[0] += '.pdf'
             return save_path
-        except IndexError:
-            return
 
     def selected(self, selection):
-        try:
+        if selection:
             path = selection[0]
             return path
-        except IndexError:
-            return
 
     def cancel():
         pass
@@ -51,15 +45,18 @@ class MainScreen(Screen):
     def merge():
         pass
 
-    #TODO: refactor IndexError handling
+    def convert(self):
+        path = self.select_file()
+        if path:
+            image = Image.open(path[0])
+            pdf_bytes = img2pdf.convert(image.filename)
+            save_path = self.save_file()
 
-    def convert(self,path, save_path):
-        image = Image.open(path[0])
-        pdf_bytes = img2pdf.convert(image.filename)
-        file = open(str(save_path[0]), 'wb')
-        file.write(pdf_bytes)
-        image.close()
-        file.close()
+            if save_path:
+                file = open(str(save_path[0]), 'wb')
+                file.write(pdf_bytes)
+                image.close()
+                file.close()
 
     def split():
         pass
